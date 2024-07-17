2866014c0914294cb6e522ee8c00c4bd6d7c911e
gabrielaspooky <gabrielanava01@gmail.com>
2024-07-17 18:36:55 +0200

    reordered divs

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -14,8 +14,6 @@ const App = () => {
       </>
       <>
     <DrillingOne />
-    <DrillingTwo />
-    <DrillingThree />
 </>
     </div>
   )
--- my-app/src/Components/DrillingOne.jsx
+++ my-app/src/Components/DrillingOne.jsx
@@ -2,12 +2,15 @@
 // la función va en el segundo
 
 import React from 'react';
+import DrillingTwo from './DrillingTwo';
+
 
 const DrillingOne = ({ x }) => {
   return (
     <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
       <h2>ComponentA</h2>
       <p>X = {x}</p>
+<DrillingTwo />
     </div>
   );
 };
